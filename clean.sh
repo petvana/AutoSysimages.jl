883cbc568c93c73194c3f3ce1203e8bc254d2cd6
Petr Vana <petvana@centrum.cz>
2022-08-28 21:43:27 +0200

    Udpate clean script

--- clean.sh
+++ clean.sh
@@ -2,3 +2,6 @@
 
 # Thish removes all files produced by AutoSysimages.jl
 rm -rf ~/.julia/asysimg/
+
+rm -f ~/.julia/environments/v*/SysimagePreferences.toml
+rm -f examples/*/SysimagePreferences.toml
