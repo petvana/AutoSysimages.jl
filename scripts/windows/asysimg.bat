599942468fdcb4f324a48cf014dea7b46933800a
Petr Vana <petvana@centrum.cz>
2022-09-19 14:04:06 +0200

    Try to fix arguments for Windows

--- scripts/windows/asysimg.bat
+++ scripts/windows/asysimg.bat
@@ -1,4 +1,4 @@
 @echo off
 set JULIA=julia.exe
-for /f "tokens=1-4" %%i in ('%JULIA% -e "using AutoSysimages; print(julia_args()); exit()"') do set A=%%i %%j %%k %%l
+for /f "tokens=1-4" %%i in ('%JULIA% -e "using AutoSysimages; print(julia_args()); exit()" %*') do set A=%%i %%j %%k %%l
 %JULIA% %A% %*
