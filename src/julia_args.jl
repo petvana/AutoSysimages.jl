60c776481832256bce0713959a6a88e32d88c1b1
Petr Vana <petvana@centrum.cz>
2022-10-05 15:29:59 +0200

    Fix julia_args.jl for the case of non-existing active directory.

--- src/julia_args.jl
+++ src/julia_args.jl
@@ -1,16 +1,18 @@
 # This mimics the `julia_args` function for fast startup.
 # It needs to be kept in sync with the functions!
 
+startfile = joinpath(@__DIR__, "start.jl")
+
 projpath = Base.active_project()
 hash_name = string(Base._crc32c(projpath), base = 62, pad = 6)
 adir = joinpath(DEPOT_PATH[1], "asysimg", "$VERSION", hash_name)
+
 if isdir(adir)
     files = readdir(adir, join = true)
     sysimages = filter(x -> endswith(x, ".so"), files) |> sort
     image =  isempty(sysimages) ? nothing : sysimages[end]
-    startfile = joinpath(@__DIR__, "start.jl")
     println((isnothing(image) ? "" : " -J $image") * " -L $startfile")
-    exit()
 else
-    exit(1)
+    println(" -L $startfile")
 end
+exit()
