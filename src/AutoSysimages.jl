8f8dd691b4e7318f6e1a0e68edaa6222884e586a
Petr Vana <petvana@centrum.cz>
2022-10-05 12:14:40 +0200

    Experiment

--- src/AutoSysimages.jl
+++ src/AutoSysimages.jl
@@ -25,6 +25,42 @@ include("build-ChainedSysimages.jl")
 # This should not happen before v1.9+ becomes LTS.
 include("pkgversion.jl")
 
+module PrecompileStagingArea
+    let
+        status_true = status_false = status_fail = 0
+        for statement in eachline(joinpath(@__DIR__, "pacakge-precompile.jl"))
+            # println(statement)
+            # The compiler has problem caching signatures with `Vararg{?, N}`. Replacing
+            # N with a large number seems to work around it.
+            statement = replace(statement, r"Vararg{(.*?), N} where N" => s"Vararg{\\1, 100}")
+            #statement = "precompile($statement)"
+            try
+                Base.include_string(PrecompileStagingArea, "tmp = " * statement)
+                if PrecompileStagingArea.tmp
+                    #printstyled("\r", statement, "\n", color = :green)
+                    status_true += 1
+                else
+                    #printstyled("\r", statement, "\n", color = :yellow)
+                    status_false += 1
+                end
+            catch
+                #printstyled("\r", statement, "\n", color = :red)
+                status_fail += 1
+                # See julia issue #28808
+                @debug "failed to execute \$statement"
+            end
+            print("\r Compilation (")
+            printstyled(status_true, color = status_true > 0 ? :green : :normal )
+            print(",")
+            printstyled(status_false, color = status_false > 0 ? :yellow : :normal )
+            print(",")
+            printstyled(status_fail, color = status_fail > 0 ? :red : :normal )
+            print(")     ")
+        end
+        println()
+    end
+end
+
 precompiles_file = ""
 is_asysimg = false
 
